058b3fe55dc56c7957b39a2b11bd222e155c213a
DivineEnder <qwertydraw@gmail.com>
2017-09-22 02:31:24 -0400

    Version .2
    Generalized the matching regex strings to match more foot inch generalizations. Also added separate individual matching strings to convert only feet and inches to the correct Shaeish height. Finally, started to keep track of nodes that had been edited. This is mostly to stop infinite looping across nodes.

--- content.js
+++ content.js
@@ -1,6 +1,7 @@
 function strHeightToNum(str)
 {
 	str = str.split(' ').join("");
+	str = str.split(',').join("");
 	console.log("Parsing \'" + str + "\'");
 	var feet = parseInt(str.match(/\d+/gi)[0], 10);
 	console.log("Integer feet = " + feet);
@@ -32,12 +33,15 @@ function computeShaeishHeightDiff(height)
 	if (totalDiff > 0)
 		return strDiff + " taller than Shaeish";
 	if (totalDiff === 0)
-		return "1 Saheish";
+		return "1 Shaeish";
 }
 
 function replaceElementText(elements)
 {
-	var matcher = /(\d+(\u2032|')[ ]?\d+(\u2033|")?)|(\d+ (foot|feet)( |, )\d+ (inches|inch))/gi;
+	var footMatcher = /\d+[,?\d+]*[ |-]?(\u2032|'|ft|ft\.|foot|feet)/gi;
+	var inchMatcher = /\d+[,?\d+]*[ |-]?(\u2033|"|in|in\.|inch|inches)/gi;
+	var totalMatcher = /\d+[,?\d+]*[ |-]?(\u2032|'|ft|ft\.|foot|feet)[ |,|, ]?\d+[,?\d+]*[ |-]?(\u2033|"|in|in\.|inch|inches)/gi;
+	// var matcher = /(\d+(\u2032|')[ ]?)|(\d+ (foot|feet|ft)( |, )\d+ (inches|inch|in))|(\d+ (foot|feet|ft|ft.))/gi;
 
 	for (var i = 0; i < elements.length; i++) {
 	    var element = elements[i];
@@ -45,23 +49,55 @@ function replaceElementText(elements)
 	    for (var j = 0; j < element.childNodes.length; j++) {
 	        var node = element.childNodes[j];
 
-	        if (node.nodeType === 3) {
+	        if (node.nodeType === 3 && cNodes.indexOf(node) === -1) {
 	            var text = node.nodeValue;
-				var matchedText = text.match(matcher);
+				var replacementText = text;
+				var matchedText = text.match(totalMatcher)
 
-				if (matchedText !== null)//text.match(/.*\d+(\u2032|')[ ]?\d+(\u2033|")?.*/))
+				if (matchedText !== null)
 				{
-					var replacedText = text.replace(matcher, computeShaeishHeightDiff(strHeightToNum(matchedText[0])));
+					console.log("Matched: " + matchedText);
+					for (var i = 0; i < matchedText.length; i++)
+						replacementText = replacementText.replace(matchedText[i], computeShaeishHeightDiff(strHeightToNum(matchedText[i])));
+				}
+				else
+				{
+					var matchedFootText = text.match(footMatcher);
+					var matchedInchText = text.match(inchMatcher);
 
-					if (replacedText !== text) {
-		                element.replaceChild(document.createTextNode(replacedText), node);
-		            }
+					if (matchedFootText !== null)
+					{
+						console.log("Matched foot: " + matchedFootText);
+						for (var i = 0; i < matchedFootText.length; i++)
+						{
+							var feet = parseInt(matchedFootText[i].match(/\d+[,?\d+]*/gi)[0].split(",").join(""), 10);
+							replacementText = replacementText.replace(matchedFootText[i], computeShaeishHeightDiff([feet, 0]));
+						}
+					}
+					if (matchedInchText !== null)
+					{
+						console.log("Matched inch: " + matchedInchText);
+						for (var i = 0; i < matchedInchText.length; i++)
+						{
+							var inches = parseInt(matchedInchText[i].match(/\d+[,?\d+]*/gi)[0].split(",").join(""), 10);
+							replacementText = replacementText.replace(matchedInchText[i], computeShaeishHeightDiff([0, inches]));
+						}
+					}
+				}
+
+				if (replacementText !== text) {
+					console.log("Replaced child");
+					var createdNode = document.createTextNode(replacementText);
+					cNodes.push(createdNode);
+
+					element.replaceChild(createdNode, node);
 				}
 	        }
 	    }
 	}
 }
 
+cNodes = [];
 replaceElementText(document.getElementsByTagName('div'));
 replaceElementText(document.getElementsByTagName('span'));
 replaceElementText(document.getElementsByTagName('em'));
